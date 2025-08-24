13207cd8ebeb893220db409ae9ec27afbc1ac5e0
SornchaiTheDev <sornchaithedev@gmail.com>
2025-08-24 21:40:49 +0700

    chore: delete participant from event if user has been deleted

--- internal/migrations/20250824070438_add_user_code_to_participants.sql
+++ internal/migrations/20250824070438_add_user_code_to_participants.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE participants ADD COLUMN user_code VARCHAR(10);
-ALTER TABLE participants ADD CONSTRAINT fk_user_code FOREIGN KEY (user_code) REFERENCES users(code);
+ALTER TABLE participants ADD CONSTRAINT fk_user_code FOREIGN KEY (user_code) REFERENCES users(code) ON DELETE CASCADE;
 -- +goose StatementEnd
 
 -- +goose Down
